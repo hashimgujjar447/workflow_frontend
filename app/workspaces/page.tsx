f876a85d8da3aa165ee9bb8bc4d8d3b7c2171f3b
Muhammad hashim <hashimgujjar4447@gmail.com>
2026-03-16 19:40:58 +0500

    feat: initial commit

--- app/workspaces/page.tsx
+++ app/workspaces/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+'use client'
+import TopBar from '@/components/layouts/TopBar'
+import React, { useState } from 'react'
 
+export interface IWorkspace {
+  title: string
+  description: string
+}
 const page = () => {
+  const [workspaces,setWorkspaces]=useState<IWorkspace[]>([
+    {
+      title:"First Workspace",
+      description:"This is the first workspace"
+    },
+       {
+      title:"First Workspace",
+      description:"This is the first workspace"
+    },
+       {
+      title:"First Workspace",
+      description:"This is the first workspace"
+    }
+  ]);
   return (
-    <div>page</div>
+    <div className='p-4'>
+      <TopBar workspaces={workspaces}   />
+    </div>
   )
 }
 
--- components/layouts/TopBar.tsx
+++ components/layouts/TopBar.tsx
@@ -1,11 +1,21 @@
 import { BellRing } from 'lucide-react'
 import React from 'react'
+import { IWorkspace } from '@/app/workspaces/page'
+interface ITopbar{
+    workspaces?:IWorkspace[]
+}
 
-const TopBar = () => {
+const TopBar = ({workspaces}:ITopbar) => {
   return (
     <div className='flex items-center justify-between'>
         <div>
-            <h1 className='font-bold text-lg'>DashBoard</h1>
+           {workspaces ?<select className='border-border border outline-none p-1'>
+            <option value="all_workspaces">Your Workspaces</option>
+            {workspaces.map((workspace)=>(
+                <option value={workspace.title}>{workspace.title}</option>
+            ))}
+           
+           </select>:<h1 className='font-bold text-lg'>DashBoard</h1>}
         </div>
          <div className='flex items-center gap-x-4'>
         <input type="text" className='outline-none border border-border p-1 rounded' placeholder='Search' />
